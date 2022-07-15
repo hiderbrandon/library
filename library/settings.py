8ce026df23b37464035bbfb32e8955fc6f822964
ḧiderbrandon¨ <hiderbrandon@gmail.com>
2022-07-15 18:40:37 -0500

    sitax error fixed

--- library/settings.py
+++ library/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import environ
-# Initialise environment
-variablesenv = environ.Env()
+
+# Initialise environment Variables
+env = environ.Env()
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #this info is on .env
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
