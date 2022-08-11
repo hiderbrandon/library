ae5d8e0727316ece7c6530967b9a0bd013cf5221
ḧiderbrandon¨ <hiderbrandon@gmail.com>
2022-08-10 19:28:26 -0500

    couple field added to book model

--- book/models.py
+++ book/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=250)
     subtitle = models.CharField(max_length=250)
+    description = models.TextField(max_length=600 , blank=True )
+    image = models.URLField(max_length=300 , blank=True )
     author = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13)
 
